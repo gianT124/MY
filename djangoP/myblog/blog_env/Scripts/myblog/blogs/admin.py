d21bbded2bc8fb63f78ab9ed9f2983da65ee05b3
gianT <957279280@qq.com>
2022-02-16 14:47:06 +0800

    blogdemo

---
+++ djangoP/myblog/blog_env/Scripts/myblog/blogs/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from blogs.models import Banner, Post, BlogCategory, Tags, FriendlyLink
+
+# Register your models here.
+
+admin.site.register(Banner)
+# admin.site.register(Post)
+admin.site.register(BlogCategory)
+admin.site.register(Tags)
+admin.site.register(FriendlyLink)
+
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = (
+            'js/editor/kindeditor-all.js',
+            'js/editor/config.js',
+        )
+
+
+admin.site.register(Post, PostAdmin)
